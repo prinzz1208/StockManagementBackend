e47cd7c2c2f6097c16f33731e65a39848bda8c60
Priyanshu Agarwal <priyanshu81212@gmail.com>
2021-09-20 23:58:31 +0530

    added database connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
+from .models import Base
+from .database import engine
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy.sql.sqltypes import Integer, String
+from .database import Base
+from sqlalchemy import Column
+class User(Base):
+  __tablename__ = "users"
+  id = Column(Integer, primary_key=True,index=True)
+  firstName = Column(String)
+  lastName = Column(String)
+  email = Column(String)
+  password = Column(String)
